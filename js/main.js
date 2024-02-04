b32e52453623ae05fbc66d68b2b6b3fa1f4b7846
AdiSabban10 <adialon050@gmail.com>
2024-02-04 16:21:19 +0200

    Limit sphere diameter

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ var gBallSize = 100
 function onBallClick() {
     var elBall = document.querySelector('.ball')
     gBallSize += 50
-
+    if (gBallSize > 400) gBallSize = 100
+    
     elBall.style.width = gBallSize + 'px' 
     elBall.style.height = gBallSize + 'px'
 
